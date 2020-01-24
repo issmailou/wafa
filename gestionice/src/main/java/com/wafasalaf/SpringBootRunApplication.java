ebe8e113d1a7b6bbc0551ea50f6c243bba2cedf0
OUARDASS ISSMAIL <OUARDASS ISSMAIL@SD06-J7VBRZ2.wafasalaf.ma>
2020-01-24 16:10:34 +0100

    test

--- gestionice/src/main/java/com/wafasalaf/SpringBootRunApplication.java
+++ gestionice/src/main/java/com/wafasalaf/SpringBootRunApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  
 @SpringBootApplication
+/**
+ * 
+ * @author OUARDASS ISSMAIL
+ *
+ */
 public class SpringBootRunApplication {
  
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRunApplication.class, args);
     }
+    private void cc() {
+		// TODO Auto-generated method stub
+
+	}
 }
